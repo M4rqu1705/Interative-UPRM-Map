ebd0189881afeb0f3ee5e89db7acb2cb328f2027
M4rqu1705 <m4rc05.dev@gmail.com>
2021-08-24 06:05:37 -0400

    Configured project and can now display image

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-debugger, no-console */
+import React from "react";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        alt="University of Puerto Rico - Mayagüez Campus map"
+        src="assets/Mapa_UPRM.png"
+      ></img>
     </div>
   );
 }
